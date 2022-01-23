cab00742483597b66fecd76b4176fb285da0531c
Dltmd202 <dltmd202@gmail.com>
2022-01-24 00:43:10 +0900

    Feat: Add function for waiting search in WaitingSearch condition

--- src/main/java/vacstage/reserve/repository/WaitingRepository.java
+++ src/main/java/vacstage/reserve/repository/WaitingRepository.java
@@ -1,11 +1,19 @@
 package vacstage.reserve.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ObjectUtils;
+import vacstage.reserve.constant.WaitingStatus;
+import vacstage.reserve.domain.waiting.QWaiting;
 import vacstage.reserve.domain.waiting.Waiting;
+import vacstage.reserve.domain.waiting.WaitingSearch;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+
+import static vacstage.reserve.domain.waiting.QWaiting.waiting;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,8 +29,24 @@ public class WaitingRepository {
         return em.find(Waiting.class, id);
     }
 
-    public List<Waiting> findAll(){
-        return em.createQuery("select w from Waiting w", Waiting.class)
-                .getResultList();
+    public List<Waiting> findAll(WaitingSearch waitingSearch){
+        QWaiting waiting = QWaiting.waiting;
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        return query
+                .select(waiting)
+                .from(waiting)
+                .where(waitingStatusLike(waitingSearch.getWaitingStatus()),
+                        waiting.date.after(waitingSearch.getSearchFromDateTime()),
+                        waiting.date.before(waitingSearch.getSearchToDateTime()))
+                .limit(waitingSearch.getLimit())
+                .fetch();
+    }
+
+    private BooleanExpression waitingStatusLike(WaitingStatus waitingStatus){
+        if(ObjectUtils.isEmpty(waitingStatus)){
+            return null;
+        }
+        return waiting.waitingStatus.eq(waitingStatus);
     }
 }
