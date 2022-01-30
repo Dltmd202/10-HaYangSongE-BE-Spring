d474c3d9300723e61944e92cd7ca3205c39eff95
Dltmd202 <dltmd202@gmail.com>
2022-01-30 23:35:57 +0900

    Fix: RestaurantRepository to extends JpaRepository (#19)

--- src/main/java/vacstage/reserve/repository/RestaurantRepository.java
+++ src/main/java/vacstage/reserve/repository/RestaurantRepository.java
@@ -1,30 +1,9 @@
 package vacstage.reserve.repository;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import vacstage.reserve.domain.Restaurant;
 
-import javax.persistence.EntityManager;
-import java.util.List;
-
-@Repository
-@RequiredArgsConstructor
-public class RestaurantRepository {
-
-    private final EntityManager em;
-
-    public void save(Restaurant restaurant){
-        em.persist(restaurant);
-    }
-
-    public Restaurant findById(Long id){
-        return em.find(Restaurant.class, id);
-    }
-
-    public List<Restaurant> findAll(){
-        return em.createQuery("select r from Restaurant r", Restaurant.class)
-                .getResultList();
-    }
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
 
 }
